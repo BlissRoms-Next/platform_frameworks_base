4b55a7dfdf5401a11c29a0c6bce4107ac5194ea0
rohan <purohit.rohan@gmail.com>
2019-10-28 15:59:05 +0530

    Revert "base: Utils: Add api to toggle flashlight and check if device has flashlight"
    This reverts commit e459dca1ae57743fa398ea21ef1482da02a0666f.

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -119,7 +119,6 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     private static final int MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW = 48 << MSG_SHIFT;
     private static final int MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW = 49 << MSG_SHIFT;
     private static final int MSG_SET_AUTOROTATE_STATUS         = 50 << MSG_SHIFT;
-    private static final int MSG_TOGGLE_CAMERA_FLASH           = 50 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -284,7 +283,6 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         default void showInDisplayFingerprintView() { }
         default void hideInDisplayFingerprintView() { }
         default void setAutoRotate(boolean enabled) { }
-        default void toggleCameraFlash() { }
 
         /**
          * @see IStatusBar#onDisplayReady(int)
@@ -808,14 +806,6 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     }
 
     @Override
-    public void toggleCameraFlash() {
-        synchronized (mLock) {
-            mHandler.removeMessages(MSG_TOGGLE_CAMERA_FLASH);
-            mHandler.sendEmptyMessage(MSG_TOGGLE_CAMERA_FLASH);
-        }
-    }
-
-    @Override
     public void onDisplayReady(int displayId) {
         synchronized (mLock) {
             mHandler.obtainMessage(MSG_DISPLAY_READY, displayId, 0).sendToTarget();
@@ -1143,11 +1133,6 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
                 case MSG_SET_AUTOROTATE_STATUS:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).setAutoRotate(msg.arg1 != 0);
-                    }
-                    break;
-                case MSG_TOGGLE_CAMERA_FLASH:
-                    for (int i = 0; i < mCallbacks.size(); i++) {
-                        mCallbacks.get(i).toggleCameraFlash();
                     }
                     break;
             }
